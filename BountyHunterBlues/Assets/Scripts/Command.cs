222eccd7ea9082d3758370b82548f906d00a33a6
nathanielballinger <nathanielballinger@utexas.edu>
2016-09-03 19:44:55 -0500

    Basic code framework for Commands and Actors

---
+++ BountyHunterBlues/Assets/Scripts/Command.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public abstract class Command : MonoBehaviour {
+
+    public abstract void execute();
+
+}
+
+public class MoveCommand : Command
+{
+    public override void execute()
+    {
+
+    }
+}
+
+public class MeleeAttackCommand : Command
+{
+    public override void execute()
+    {
+
+    }
+}
+
+public abstract class RangedAttackCommand : Command
+{
+}
+
+public class RangedShotCommand : Command
+{
+    public override void execute()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class RangedSpecialCommand : Command
+{
+    public override void execute()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class InteractCommand : Command
+{
+    public override void execute()
+    {
+        
+    }
+}
+
+public class AimCommand : Command
+{
+    public override void execute()
+    {
+        
+    }
+}
