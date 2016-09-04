2e1aaf15315284d66be5f33c1de62fdfa856ce02
kishkumen123 <rizik944@gmail.com>
2016-09-04 02:37:35 -0500

    Add files via upload
    Incomplete, must discuss.

---
+++ BountyHunterBlues/Assets/Scripts/GameLoop.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLoop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		load_scene()
+		load_level()
+		load_npcs()
+		load_items()
+		load_player()
+	}
+	
+	// Update is called once per frame
+	player_loop(){
+		check_player_movement()
+		check_player_action(){
+			check_slash(){
+				return result
+			}
+			check_fire(){
+				return result
+			}
+			check_interact(){
+				return result
+			}
+		}
+	}
+
+	npc_loop(){
+		animate_AI(){
+			animate_nearby_AI()
+			run_AI_detection(){
+				if_ray_hit(){
+					run_AI_alert_sequence(){
+						yellow(){
+							investigate()
+						}
+						red(){
+							alert_or_attack()
+						}
+					}
+				}
+				else(){
+					green()
+				}
+			}
+			run_AUDIO_detection(){
+				if_sound_detected(){
+					run_AI_alert_sequence(){
+						yellow(){
+							investigate()
+						}
+						red(){
+							alert_or_attack()
+						}
+					}
+				}
+				else(){
+					green()
+				}
+			}
+		}
+		check_AI_action(){
+			check_slash(){
+				return result
+			}
+			check_fire(){
+				return result
+			}
+			check_interact(){
+				return result
+			}
+		}
+	}
+
+	object_loop(){
+		check_interactable_objects(){
+			check_interaction(){
+				pick_up()
+				use()
+				switch()
+				proximity()
+			}
+		}
+	}
+
+	objective_loop(){
+		objective_complete(){
+			return result
+		}
+		scene_exit(){
+			check_objective_completion(){
+				if_complete(){
+					return result
+				}
+				else(){
+					return result
+				}
+			}
+		}
+	}
+
+	void Update () {
+		player_loop()
+		npc_loop()
+		object_loop()
+		objective_loop()
+
+		if(objective_complete()){
+			scene_transition()
+		}
+	}
+}
