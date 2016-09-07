c0fa073eba9ab27bece7bd8e6f550a08256b78ea
nathanielballinger <nathanielballinger@utexas.edu>
2016-09-07 12:38:04 -0500

    Added endgame restart trigger

---
+++ BountyHunterBlues/Assets/Scripts/RestartGameTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RestartGameTrigger : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "GameActor" && col.GetComponent<GameActor>() is PlayerActor)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
