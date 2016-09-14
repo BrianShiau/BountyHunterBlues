da2c9026009831ece933cf55f3323076ab112c77
nathanielballinger <nathanielballinger@utexas.edu>
2016-09-14 14:10:01 -0500

    Turned off enemy color change, turned on laser color change.

--- BountyHunterBlues/Assets/Scripts/Laser.cs
+++ BountyHunterBlues/Assets/Scripts/Laser.cs
@@ -10,6 +10,13 @@ public class Laser : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        AIActor actor = GetComponentInParent<AIActor>();
+        if (actor.alertness == State.GREEN)
+            GetComponent<SpriteRenderer>().color = Color.green;
+        else if (actor.alertness == State.YELLOW)
+            GetComponent<SpriteRenderer>().color = Color.yellow;
+        else if (actor.alertness == State.RED)
+            GetComponent<SpriteRenderer>().color = Color.red;
         float angle = Mathf.Atan2(-1 * GetComponentInParent<AIActor>().faceDir.x, -1 * GetComponentInParent<AIActor>().faceDir.y) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, -1 * Vector3.forward);
     }
