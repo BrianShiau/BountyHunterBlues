cb341fe2bc0d7ddcb17185fee10479f8c4f06013
nathanielballinger <nathanielballinger@utexas.edu>
2016-09-14 01:31:51 -0500

    New run animations. Setup basic barrel rotation (doesn't swap out sprites)

---
+++ BountyHunterBlues/Assets/Scripts/BarrelRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float angle = Mathf.Atan2(-1* GetComponentInParent<AIActor>().faceDir.x, -1 * GetComponentInParent<AIActor>().faceDir.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, -1 * Vector3.forward);
+        
+	}
+}
