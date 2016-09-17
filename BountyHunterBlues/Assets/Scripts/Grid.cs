7108786421a2016ae6824c5cc6bb6ba0ee8a94b7
nathanielballinger <nathanielballinger@utexas.edu>
2016-09-17 16:37:53 -0500

    WIP Grid for Pathfinding.

---
+++ BountyHunterBlues/Assets/Scripts/Grid.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GridPoint
+{
+    public int X;
+    public int Y;
+    public GridPoint(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+public class Grid : MonoBehaviour {
+
+
+    public float unitsize;
+    
+    public Node[,] nodes;
+    public int width;
+    public int height;
+
+    private float worldWidth;
+    private float worldHeight;
+
+
+	// Use this for initialization
+	void Start () {
+
+        worldWidth = transform.localScale.x;
+        worldHeight = transform.localScale.y;
+        width = (int)(worldWidth / unitsize);
+        height = (int)(worldHeight / unitsize);
+
+        nodes = new Node[width, height];
+
+        for (int x = 0; x < width; ++x)
+            for (int y = 0; y < height; ++y)
+                nodes[x, y] = new Node(x, y, gridToWorld(x, y), this);
+
+        for (int x = 0; x < width; ++x)
+            for (int y = 0; y < height; ++y)
+                nodes[x, y].setupConnections();
+
+        for (int x = 0; x < width; ++x)
+            for (int y = 0; y < height; ++ y)
+                nodes[x, y].cullInactiveConnections();
+
+    }
+	
+	public Vector2 gridToWorld(int x, int y)
+    {
+        Vector2 result = new Vector2();
+
+        return result;
+        
+    }
+
+    public GridPoint worldToGrid(Vector2 worldPoint)
+    {
+        return new GridPoint(0, 0);
+    }
+
+    public bool inBounds(Vector2 point)
+    {
+        return point.x >= transform.position.x && point.x <= transform.position.x + worldWidth
+            && point.y >= transform.position.y && point.y <= transform.position.y + worldHeight;
+    }
+}
---
+++ BountyHunterBlues/Assets/Scripts/Node.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NodeConnection
+{
+
+    public Node source;
+    public Node destination;
+    public bool active;
+
+    public NodeConnection(Node source, Node destination, Grid grid)
+    {
+        this.source = source;
+        this.destination = destination;
+        active = true;
+    }
+
+}
+
+public class Node {
+
+    public bool active; // node becomes active with more than 2 connections
+
+    public GridPoint point;
+
+    public Vector2 worldPosition;
+
+    private Grid grid;
+    private List<NodeConnection> connections;
+
+    public Node(int x, int y, Vector2 position, Grid grid)
+    {
+        worldPosition = position;
+        point = new GridPoint(x, y);
+        this.grid = grid;
+    }
+
+    public void setupConnections()
+    {
+        float diagonalDist = Mathf.Sqrt(Mathf.Pow(grid.unitsize, 2) + Mathf.Pow(grid.unitsize, 2));
+        if(point.X > 0)
+        {
+           createConnection(grid.nodes[point.X - 1, point.Y]);
+            if (point.Y < grid.height - 1)
+                createConnection(grid.nodes[point.X - 1, point.Y + 1]);
+            if (point.Y > 0)
+                createConnection(grid.nodes[point.X - 1, point.Y - 1]);
+        }
+        if (point.X < grid.width - 1)
+        {
+            createConnection(grid.nodes[point.X + 1, point.Y]);
+            if (point.Y < grid.height - 1)
+                createConnection(grid.nodes[point.X + 1, point.Y + 1]);
+            if (point.Y > 0)
+                createConnection(grid.nodes[point.X + 1, point.Y - 1]);
+        }
+        if (point.Y < grid.height - 1)
+            createConnection(grid.nodes[point.X, point.Y + 1]);
+        if (point.Y > 0)
+            createConnection(grid.nodes[point.X, point.Y - 1]);
+
+        active = connections.Count > 2;
+    }
+
+    public void cullInactiveConnections()
+    {
+        foreach (NodeConnection connection in connections)
+        {
+            if (!connection.destination.active)
+                connection.active = false;
+        }
+    }
+
+    private bool createConnection(Node destination)
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(worldPosition, destination.worldPosition - worldPosition, grid.unitsize);
+        if ((hit.collider == null || hit.collider.tag == "GameActor") && grid.inBounds(destination.worldPosition))
+        {
+            connections.Add(new NodeConnection(this, destination, grid));
+            return true;
+        }
+
+        return false;
+    }
+
+}
