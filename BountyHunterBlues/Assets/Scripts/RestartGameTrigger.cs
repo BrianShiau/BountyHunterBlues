be423b9625ac24f045c9f497d20305bdec8c7d82
BrianShiau <brian-s@utexas.edu>
2016-09-17 21:54:13 -0500

    some load level stuff. waiting on whether we want black fades

--- BountyHunterBlues/Assets/Scripts/RestartGameTrigger.cs
+++ BountyHunterBlues/Assets/Scripts/RestartGameTrigger.cs
@@ -6,8 +6,11 @@ public class RestartGameTrigger : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.tag == "GameActor" && col.GetComponent<GameActor>() is PlayerActor)
-			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Additive);
+		if (col.tag == "GameActor" && col.GetComponent<GameActor> () is PlayerActor) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1, LoadSceneMode.Additive);
+			SceneManager.SetActiveScene (SceneManager.GetSceneAt(SceneManager.GetActiveScene ().buildIndex + 1));
+			Destroy (this.gameObject);
+		}
 		
     }
 }
