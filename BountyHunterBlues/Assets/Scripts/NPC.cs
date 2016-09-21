9b771f77f9ce6c5fd83c18a607fb6d5bf2c344c1
BrianShiau <brian-s@utexas.edu>
2016-09-21 14:29:41 -0500

    updated transition scene

---
+++ BountyHunterBlues/Assets/Scripts/NPC.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour, Interactable {
+
+	public bool currentlyTalking;
+
+	// Use this for initialization
+	void Start () {
+		currentlyTalking = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void runInteraction()
+	{
+		if (!currentlyTalking) {
+			
+		}
+	}
+}
