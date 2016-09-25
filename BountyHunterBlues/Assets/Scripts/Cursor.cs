91a1a178e9a0519e6d8005b709f3ad8b4b162bf2
BrianShiau <brian-s@utexas.edu>
2016-09-24 20:21:11 -0500

    added custom cursor

---
+++ BountyHunterBlues/Assets/Scripts/Cursor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cursor : MonoBehaviour {
+	public Texture2D cursorTexture;
+	public CursorMode cursorMode = CursorMode.Auto;
+
+
+	// Use this for initialization
+	void Start () {
+		UnityEngine.Cursor.SetCursor(cursorTexture, Vector2.zero, cursorMode);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
