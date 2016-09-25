907bb78e66b453d55e7f4e80f82d8b71fa8f16bc
BrianShiau <brian-s@utexas.edu>
2016-09-24 20:01:10 -0500

    made interaction UI more visible. test in rest area. waiting for art

---
+++ BountyHunterBlues/Assets/Scripts/TutorialHint.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TutorialHint : MonoBehaviour {
+
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		StartCoroutine (FlashText());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator FlashText(){
+		while (true) {
+			text.color = new Color (text.color.r, text.color.g, text.color.b, .2f);
+			for (int i = 0; i < 8; i++) {
+				text.color = new Color (text.color.r, text.color.g, text.color.b, text.color.a + .1f);
+				yield return new WaitForSeconds (.3f * (.1f / text.color.a));
+			}
+			for (int i = 0; i < 8; i++) {
+				text.color = new Color (text.color.r, text.color.g, text.color.b, text.color.a - .1f);
+				yield return new WaitForSeconds (.3f * (.1f / text.color.a));
+			}
+		}
+	}
+}
