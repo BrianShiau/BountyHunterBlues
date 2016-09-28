b4c0659614918597be8956280a37f211c4386e21
BrianShiau <brian-s@utexas.edu>
2016-09-28 00:00:03 -0500

    rotated secondary reticule

--- BountyHunterBlues/Assets/Scripts/Cursor.cs
+++ BountyHunterBlues/Assets/Scripts/Cursor.cs
@@ -19,11 +19,13 @@ public class Cursor : MonoBehaviour {
 
 	void OnGUI(){
 		Vector2 pivot = new Vector2(Screen.width/2, Screen.height/2);
-		GUIUtility.RotateAroundPivot(45%360,pivot);
-		GUI.DrawTexture (new Rect (
-			(Event.current.mousePosition.x + Screen.width / 2) / 2 - (32 / 2),
-			(Event.current.mousePosition.y + Screen.height / 2) / 2 - (32 / 2),
-			32, 32),
-			secondaryCursorTexture);		
+		float distFromCenterX = Event.current.mousePosition.x - Screen.width / 2;
+		float distFromCenterY = Event.current.mousePosition.y - Screen.height / 2;
+		float angle = Mathf.Atan2 (distFromCenterY, distFromCenterX);
+		GUIUtility.RotateAroundPivot(90+angle*Mathf.Rad2Deg, pivot);
+
+		float x = (Event.current.mousePosition.x + Screen.width / 2) / 2 - (48 / 2);
+		float y = (Event.current.mousePosition.y + Screen.height / 2) / 2 - (48 / 2);
+		GUI.DrawTexture (new Rect (x, y, 48, 48), secondaryCursorTexture);		
 	}
 }
