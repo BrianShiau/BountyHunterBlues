a1c7c3d7ea7b70e2585aa6bdef86f334b2d477d6
BrianShiau <brian-s@utexas.edu>
2016-10-11 23:14:19 -0500

    added more patrol code to NPCActor

--- BountyHunterBlues/Assets/Scripts/NPCActor.cs
+++ BountyHunterBlues/Assets/Scripts/NPCActor.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class NPCActor : Actor{
 
@@ -11,7 +12,10 @@ public class NPCActor : Actor{
 	// Update is called once per frame
 	public override void Update () {
 		base.Update();
-		if(patrolManager.get_patrol_length() > 0){
+		if(patrolManager.get_next_patrol_point().x == Int32.MaxValue && patrolManager.get_next_patrol_point().y == Int32.MaxValue){
+			stopMove ();
+		}
+		else if(patrolManager.get_patrol_length() > 0){
 			Vector2 worldFace = patrolManager.get_next_patrol_point() - new Vector2(transform.position.x, transform.position.y);
 			worldFace.Normalize();
 			faceDir = transform.InverseTransformDirection(worldFace);
