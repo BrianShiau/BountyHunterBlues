3290d904be693018dd1d7d747bd0c7512ded119b
BrianShiau <brian-s@utexas.edu>
2016-10-23 23:10:45 -0500

    made gun pulse sizes, added extra light

--- BountyHunterBlues/Assets/Scripts/PickupGun.cs
+++ BountyHunterBlues/Assets/Scripts/PickupGun.cs
@@ -3,6 +3,18 @@ using System.Collections;
 
 public class PickupGun : MonoBehaviour {
 
+	public Vector3 startSize;
+	public Vector3 endSize;
+
+	void Start(){
+	}
+
+	void Update(){
+		//float t = (Mathf.Sin(Time.time * 10.0f) + 1) / 2.0f;
+		float t = Mathf.PingPong(Time.time * 2.0f, 1.0f);
+		transform.localScale = Vector3.Lerp (startSize, endSize, t);
+	}
+
 	void OnTriggerEnter2D(Collider2D col)
     {
         if(col.tag == "GameActor" && col.GetComponent<GameActor>() is PlayerActor)
