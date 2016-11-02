9c6be5e93dc6f2bd12e56acab5e4eaf214cb767c
kishkumen123 <rizik944@gmail.com>
2016-11-01 23:16:31 -0500

    ok

--- BountyHunterBlues/Assets/Scripts/MissileStates.cs
+++ BountyHunterBlues/Assets/Scripts/MissileStates.cs
@@ -229,8 +229,8 @@ public class AggresiveMissile : MissileState
 {
     public AggresiveMissile(MissileEnemy enemy) : base(enemy) { }
 
-    private float shoot_timer = 0;
-    private float shoot_timer_threshold = 0;
+    private float shoot_timer = 1;
+    private float shoot_timer_threshold = 1;
 
     public override void on_enter()
     {
