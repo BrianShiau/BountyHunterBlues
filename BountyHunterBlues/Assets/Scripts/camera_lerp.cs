8058c8019611d92e2eb4c13485f746aac81ae546
kishkumen123 <rizik944@gmail.com>
2016-11-06 00:11:09 -0500

    ok

---
+++ BountyHunterBlues/Assets/Scripts/camera_lerp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_lerp : MonoBehaviour {
+
+	private Vector3 camera_world_position;
+	public GameObject lerp_to;
+	// Use this for initialization
+	void Start () {
+		camera_world_position = transform.TransformPoint(transform.position);
+		//Debug.Log(transform.TransformPoint(transform.position));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
