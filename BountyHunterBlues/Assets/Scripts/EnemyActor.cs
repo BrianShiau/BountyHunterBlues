5a1296c2889cf096800e0577d9e3d82366bd3467
nathanielballinger <nathanielballinger@utexas.edu>
2016-11-08 21:05:48 -0600

    WIP variable ray origin

--- BountyHunterBlues/Assets/Scripts/EnemyActor.cs
+++ BountyHunterBlues/Assets/Scripts/EnemyActor.cs
@@ -31,6 +31,7 @@ public abstract class EnemyActor : GameActor {
     public int path_threshold;
     private bool shot;
 
+    private Transform raySource;
 	// UI Reactions
 	Animator reactionAnim;
 	int reactionStack;
@@ -75,6 +76,9 @@ public abstract class EnemyActor : GameActor {
             reactionStack = 0;
         }
 		directionPointer = transform.FindChild ("DirectionPointer").gameObject;
+
+        raySource = transform.Find("RaySource");
+        
     }
 
     public override void Update(){
@@ -287,8 +291,12 @@ public abstract class EnemyActor : GameActor {
     public override GameActor[] runVisionDetection(float fov, float sightDistance){
         PlayerActor actorObject = GameObject.FindObjectOfType<PlayerActor>();
         List<GameActor> GameActors = new List<GameActor>();
-
-        Vector2 worldVector = actorObject.transform.position - transform.position;
+        Vector3 rayOrigin;
+        if (raySource == null)
+            rayOrigin = transform.position;
+        else
+            rayOrigin = raySource.position;
+        Vector2 worldVector = actorObject.transform.position - rayOrigin;
         worldVector.Normalize();
         Vector2 toTargetDir = transform.InverseTransformDirection(worldVector);
         if (Mathf.Abs(Vector2.Angle(faceDir, toTargetDir)) < fov / 2){
