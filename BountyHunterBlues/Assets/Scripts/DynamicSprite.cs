e186eec2ab21136b70c2bc58f97fa2368f705bf2
nathanielballinger <nathanielballinger@utexas.edu>
2016-11-16 13:28:07 -0600

    Bar sprite layering

--- BountyHunterBlues/Assets/Scripts/DynamicSprite.cs
+++ BountyHunterBlues/Assets/Scripts/DynamicSprite.cs
@@ -4,14 +4,18 @@ using System.Collections;
 public class DynamicSprite : MonoBehaviour {
 
     private SpriteRenderer mySprite;
+    private Transform SpriteLayerPoint;
 
 	// Use this for initialization
 	void Start () {
         mySprite = GetComponent<SpriteRenderer>();
-	}
+        SpriteLayerPoint = transform.Find("SpriteLayerPoint");
+       
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        mySprite.sortingOrder = Mathf.RoundToInt(-1 * transform.position.y * 20);
+        float yPos = SpriteLayerPoint != null ? SpriteLayerPoint.position.y : transform.position.y;
+        mySprite.sortingOrder = Mathf.RoundToInt(-1 * yPos * 20);
     }
 }
