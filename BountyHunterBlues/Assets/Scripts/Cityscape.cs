9c84a5ce094424026e2a080693f0b05ca604bdb1
BrianShiau <brian-s@utexas.edu>
2016-11-27 21:03:59 -0600

    parallax vity background on transition 1

---
+++ BountyHunterBlues/Assets/Scripts/Cityscape.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cityscape : MonoBehaviour {
+	private GameObject player;
+	private Vector3 startingPosition;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("0_Player");
+		startingPosition = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 translation = (player.transform.position - (player.transform.position - startingPosition) / 2F);
+		transform.position = new Vector3 (translation.x, 
+			player.transform.position.y - ((player.transform.position.y - startingPosition.y)/1.5f), 
+			transform.position.z);
+	}
+}
